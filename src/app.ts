f6443b48296eea27af1683d87db800d2eabeb294
unknown <shorifali4321@gmail.com>
2024-05-25 15:42:14 +0600

    check lint by tst

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response, urlencoded } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-//midleware
+//midleware and
 app.use(cors())
 
 // parser
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Application running Perfectly')
 })
 
