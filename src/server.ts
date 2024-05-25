b22504ebaf9a69142426fb6f3d5a4a7e96683c27
unknown <shorifali4321@gmail.com>
2024-05-25 12:28:13 +0600

    lint stage

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("🛢 Databae connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('🛢 Databae connected successfully')
     app.listen(config.path, () => {
-      console.log(`Example app listening on port ${config.path}`);
-    });
+      console.log(`Example app listening on port ${config.path}`)
+    })
   } catch (error) {
-    console.log("Faild to connected database", error);
+    console.log('Faild to connected database', error)
   }
 }
 
-bootstrap();
+bootstrap()
